d72390d4ec8af6e9472ae441e2146f9fa947c52f
Hettore Eduardo <duh.barretoo@gmail.com>
2022-08-19 15:08:12 -0300

    Adiciconando exercício de soma de vetores.

--- src/somavetores/Program.java
+++ src/somavetores/Program.java
@@ -18,18 +18,18 @@ public class Program {
 		for (int i = 0; i < vetorA.length; i++) {
 			vetorA[i] = sc.nextInt();
 		}
-		
+
 		System.out.println("Digite os valores do vetor B: ");
 		for (int i = 0; i < vetorB.length; i++) {
 			vetorB[i] = sc.nextInt();
 		}
-		
+
 		System.out.println("Vetor resultado da soma de cada posicao do vetorA + vetorB: ");
 		for (int i = 0; i < vetorB.length; i++) {
 			vetorC[i] = vetorA[i] + vetorB[i];
 			System.out.println(vetorC[i]);
 		}
-		
+
 	}
 
 }
